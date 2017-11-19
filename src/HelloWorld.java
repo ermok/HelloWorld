3880c6cb65372b541239342af5f282c9393662e0
eric <eric@DESKTOP-GBR6BF8>
2017-11-19 13:51:59 +0800

    added getter and setter

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,28 @@
 
 public class HelloWorld {
 
+	String helloString;
+	
+	
+	public HelloWorld(String helloString) {
+		super();
+		setHelloString(helloString);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		HelloWorld h = new HelloWorld("testing");
+		
+		System.out.println("Hello World!" + h.getHelloString());
+		
+	}
+
+	public String getHelloString() {
+		return helloString;
+	}
+
+	public void setHelloString(String helloString) {
+		this.helloString = helloString;
 	}
 
 }
